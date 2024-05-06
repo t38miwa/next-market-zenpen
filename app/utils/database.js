139afd579b749d15d3d6c81eba10032a4db44ed5
t38miwa <t38miwa@gmail.com>
2024-05-06 10:54:12 +0900

    second-commit

---
+++ app/utils/database.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose"
+
+const connectDB = async() => {
+    try{
+        await mongoose.connect("mongodb+srv://monotein-next:XXXX@cluster0.nclxqnr.mongodb.net/nextAppDataBase?retryWrites=true&w=majority")
+        console.log("Success: Connected to MongoDB")
+    }catch(err){
+        console.log("Failure: Unconnected to MongoDB")
+        throw new Error()
+    }
+}
+
+export default connectDB
